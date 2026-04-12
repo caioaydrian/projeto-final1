f508a5cfd8993e5e8806f5d49ecf79b3208aa265
caioaydrian <caioaydrian42@gmail.com>
2026-04-12 17:52:44 -0300

    jogo completo

---
+++ primeiro-jogo/jogo-salvo/jogo-completo/scripts/project/javaScriptInEvents.js
@@ -0,0 +1,32 @@
+
+
+const scriptsInEvents = {
+
+	async Folhajogo2_Event10_Act5(runtime, localVars)
+	{
+		runtime.goToLayout("vitoria")
+	},
+
+	async Folhajogo1_Event11_Act1(runtime, localVars)
+	{
+		const nave = runtime.objects.nave1.getFirstInstance();
+		const tiro = runtime.objects.tiroig1.getFirstPickedInstance();
+		
+		if (nave && tiro) {
+		    nave.instVars.vida -= 10;
+		    tiro.destroy();
+		}
+	},
+
+	async Folhajogo1_Event12_Act4(runtime, localVars)
+	{
+		runtime.goToLayout("fase-2")
+	},
+
+	async ["Folha-Vitoria_Event2_Act1"](runtime, localVars)
+	{
+		runtime.goToLayout("tela-inicial")
+	}
+};
+
+globalThis.C3.JavaScriptInEvents = scriptsInEvents;
